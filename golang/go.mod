e9727a7d6e0063455cc2b27ffe14b8c77493d076
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-25 12:30:05 -0300

    feat: add entities

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/RianNegreiros/full-cycle-14/golang
+
+go 1.21.0
---
+++ golang/internal/freight/entity/entity.go
@@ -0,0 +1,50 @@
+package entity
+
+import "time"
+
+type Route struct {
+	ID           string
+	Name         string
+	Distance     float64
+	Status       string
+	FreightPrice float64
+	StartedAt    time.Time
+	FinishedAt   time.Time
+}
+
+func NewRoute(id, name string, distance float64) *Route {
+	return &Route{
+		ID:       id,
+		Name:     name,
+		Distance: distance,
+		Status:   "pending",
+	}
+}
+
+func (r *Route) Start(startedAt time.Time) {
+	r.Status = "started"
+	r.StartedAt = startedAt
+}
+
+func (r *Route) Finish(finishedAt time.Time) {
+	r.Status = "finished"
+	r.FinishedAt = finishedAt
+}
+
+type FreightInterface interface {
+	Calculate(route *Route)
+}
+
+type Freight struct {
+	PricePerKm float64
+}
+
+func NewFreight(pricePerKm float64) *Freight {
+	return &Freight{
+		PricePerKm: pricePerKm,
+	}
+}
+
+func (r *Freight) Calculate(route *Route) {
+	route.FreightPrice = route.Distance * r.PricePerKm
+}
