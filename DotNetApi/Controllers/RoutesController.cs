cde5c460e85e8c864ef7e92b43b08cd1a692c632
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-28 12:32:11 -0300

    feat: add Get, Patch and Delete endpoints

--- DotNetApi/Controllers/RoutesController.cs
+++ DotNetApi/Controllers/RoutesController.cs
@@ -1,22 +1,17 @@
 using DotNetApi.DTOs;
-using DotNetApi.Jobs;
 using DotNetApi.Services;
-using DotNetApi.WebSockets;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetApi.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
 public class RoutesController : ControllerBase
 {
     private readonly RoutesService _routesService;
-    private readonly RoutesGateway _routesGateway;
-    private readonly IJobQueue _jobProcessorService;
-    public RoutesController(RoutesService routesService, RoutesGateway routesGateway, IJobQueue jobProcessorService)
+    public RoutesController(RoutesService routesService)
     {
         _routesService = routesService;
-        _routesGateway = routesGateway;
-        _jobProcessorService = jobProcessorService;
     }
 
     [HttpPost]
@@ -38,11 +33,45 @@ public class RoutesController : ControllerBase
         return Ok(routes);
     }
 
-    [HttpPost("send-new-points")]
-    public IActionResult SendNewPoints([FromBody] NewPointsPayload payload)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> FindOne([FromRoute] string id)
     {
-        _routesGateway.SendMessage(payload.RouteId, payload.Lat, payload.Lng);
-        _jobProcessorService.EnqueueAsync(payload);
-        return Ok("Message sent to Socket.IO server.");
+        var route = await _routesService.FindOneAsync(id);
+        if (route == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(route);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateRoute([FromQuery] string id, [FromBody] UpdateRouteDto routeDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var updatedRoute = await _routesService.UpdateAsync(id, routeDto);
+        if (updatedRoute == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(updatedRoute);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteRoute([FromQuery] string id)
+    {
+        var deletedRoute = await _routesService.RemoveAsync(id);
+
+        if (deletedRoute)
+        {
+            return NotFound();
+        }
+
+        return Ok(deletedRoute);
     }
 }
