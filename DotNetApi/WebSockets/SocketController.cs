4495e1d8eed61e943652be11a43e5298a162d33a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-08-28 12:31:09 -0300

    feat: add socket controller

---
+++ DotNetApi/WebSockets/SocketController.cs
@@ -0,0 +1,32 @@
+using DotNetApi.Jobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetApi.WebSockets;
+
+[ApiController]
+[Route("socket.io/")]
+public class SocketController : ControllerBase
+{
+    private readonly RoutesGateway _routesGateway;
+    private readonly IJobQueue _jobProcessorService;
+
+    public SocketController(RoutesGateway routesGateway, IJobQueue jobProcessorService)
+    {
+        _routesGateway = routesGateway;
+        _jobProcessorService = jobProcessorService;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Socket.IO server is running.");
+    }
+
+    [HttpPost]
+    public IActionResult SendNewPoints([FromBody] NewPointsPayload payload)
+    {
+        _routesGateway.SendMessage(payload.RouteId, payload.Lat, payload.Lng);
+        _jobProcessorService.EnqueueAsync(payload);
+        return Ok("Message sent to Socket.IO server.");
+    }
+}
