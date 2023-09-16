60a7592c7bdd0eb5228a132d27a7c6727d143b66
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-09-16 16:41:35 -0300

    refactor: remove swagger config from Program

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -49,8 +49,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseCors("CorsPolicy");
